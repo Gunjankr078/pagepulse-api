8d10dd6b81c5ccf4d75de5c1d995242d28de5a3b
Gunjan Kumar <gunjankrs822@gmail.com>
2026-07-25 20:40:23 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/gunjan/pagepulse/PagepulseApiApplication.java
@@ -0,0 +1,13 @@
+package com.gunjan.pagepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagepulseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagepulseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gunjan/pagepulse/PagepulseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunjan.pagepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagepulseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
